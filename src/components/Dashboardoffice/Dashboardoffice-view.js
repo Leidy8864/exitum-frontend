9bbf4e4c8211e3e39873e0bbfff279813e6f1d5f
javier eduardo lecca cruzado <javier@MacBook-Pro-de-javier.local>
2019-10-22 19:51:47 -0500

    fix tamaño de la agenda

--- src/components/Dashboardoffice/Dashboardoffice-view.js
+++ src/components/Dashboardoffice/Dashboardoffice-view.js
@@ -15,11 +15,11 @@ function View() {
                 <div className="content">
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col-xl-9 col-lg-12">
+                            <div className="col-xl-8 col-lg-12">
                                 <Tree />
                                 <Cherry />
                             </div>
-                            <div className="col-xl-3 col-lg-12">
+                            <div className="col-xl-4 col-lg-12">
                                 <Diary />
                             </div>
                         </div>
