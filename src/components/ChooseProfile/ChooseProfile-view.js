9c4b6d0eb397a21996379ad034989e579474e0f4
Diego Ortega <rhaegarcode@gmail.com>
2019-10-22 20:12:56 -0500

    -Cherry y algunos cambios

--- src/components/ChooseProfile/ChooseProfile-view.js
+++ src/components/ChooseProfile/ChooseProfile-view.js
@@ -10,25 +10,25 @@ function View(props) {
     } = props;
 
     return (
-        <div className="ChooseProfile">
-            <div className="title">
-                Elige un perfil
+            <div className="ChooseProfile">
+                <div className="title">
+                    Elige un perfil
             </div>
-            <form>
-            <div className="content-radio">
-                <img className="img_impulsor" src={require("../../public/images/svg/asistente_impulsor.svg")} alt="svg"/>
-                <br/>
-                <input type="radio" name="role" id="employee" onChange={handleChangeRole} value="employee"/>
-                <label for="employee">Impulsor</label>
+                <form>
+                    <div className="content-radio">
+                        <img className="img_impulsor" src={require("../../public/images/svg/asistente_impulsor.svg")} alt="svg" />
+                        <br />
+                        <input type="radio" name="role" id="employee" onChange={handleChangeRole} value="employee" />
+                        <label for="employee">Impulsor</label>
+                    </div>
+                    <div className="content-radio">
+                        <img className="img_emprendedor" src={require("../../public/images/svg/asistente_emprendedor.svg")} alt="svg" />
+                        <br />
+                        <input type="radio" name="role" id="entrepreneur" onChange={handleChangeRole} value="entrepreneur" />
+                        <label for="entrepreneur">Emprendedor</label>
+                    </div>
+                </form>
             </div>
-            <div className="content-radio">
-                <img className="img_emprendedor" src={require("../../public/images/svg/asistente_emprendedor.svg")} alt="svg"/>
-                <br/>
-                <input type="radio" name="role" id="entrepreneur" onChange={handleChangeRole} value="entrepreneur" />
-                <label for="entrepreneur">Emprendedor</label>
-            </div>
-            </form>
-        </div>
     );
 }
 export default View;
--- src/components/Dashboardoffice/Dashboardoffice-view.js
+++ src/components/Dashboardoffice/Dashboardoffice-view.js
@@ -15,7 +15,7 @@ function View() {
                 <div className="content">
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col-xl-8 col-lg-12">
+                            <div className="col-xl-8 col-lg-12 content-two">
                                 <Tree />
                                 <Cherry />
                             </div>
